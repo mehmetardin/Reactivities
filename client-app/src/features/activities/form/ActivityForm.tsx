2681fc96ebfb9c4b0972fa8c0d4506c10775c2f3
Mehmet Ardin <mehmetardin@Mehmets-MacBook-Pro.local>
2021-08-14 22:32:53 +0300

    routing impelemtetion

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,18 +1,24 @@
 import { observer } from "mobx-react-lite";
 import { ChangeEvent } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
-
+import {v4 as uuid} from 'uuid';
 
 
 
 export default observer( function ActivityForm(){
 
-    const {activityStore} = useStore();
-    const {selectedActivity, closeForm, createActivity,updateActivity,loading} = activityStore;
+    const history = useHistory();
 
-    const initialState = selectedActivity ?? {
+    const {activityStore} = useStore();
+    const { createActivity,updateActivity,loading, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id:string}>();
+
+    const [activity, setActivity] = useState({
         id: '',
         title: '',
         category: '',
@@ -20,12 +26,24 @@ export default observer( function ActivityForm(){
         date: '',
         city: '',
         venue: ''
-    }
+    });
 
-    const [activity, setActivity] = useState(initialState);
+   useEffect(() =>{
+       if(id) loadActivity(id).then(activity => setActivity(activity!))
+   },[id, loadActivity])
+
+    
 
     function handleSubmit(){
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if(activity.id.length === 0){
+            let newActivity ={
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
+        } else{
+            updateActivity(activity).then(()=> history.push(`/activities/${activity.id}`));
+        }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
@@ -33,6 +51,7 @@ export default observer( function ActivityForm(){
         setActivity({...activity,[name] : value})
     }
 
+    if(loadingInitial) return <LoadingComponent content='Loading Activities...' />
 
     return(
         <Segment clearing>
@@ -44,7 +63,7 @@ export default observer( function ActivityForm(){
                <Form.Input placeholder='City' value = {activity.city} name='city' onChange={handleInputChange}/>
                <Form.Input placeholder='Venue' value = {activity.venue} name='venue' onChange={handleInputChange}/>
                <Button loading = {loading} floated ='right' positive type='submit' content='Submit'/>
-               <Button onClick={closeForm} floated ='right' type='button' content='Cancel'/>
+               <Button as ={Link} to ={'/activities'} floated ='right' type='button' content='Cancel'/>
                
             </Form>
         </Segment>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './/app/layout/styles.css';
 import App from './app/layout/App';
@@ -8,7 +9,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <StoreContext.Provider value ={store}>
+    <BrowserRouter>
     <App />
+    
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
